94326a3e71c3903bbbf33ad1a4c7def60086314e
coloradomedia <piotrek.majkrzak@gmail.com>
2019-05-29 21:38:06 +0200

    gtag google

--- header.php
+++ header.php
@@ -2,6 +2,16 @@
 <html <?php language_attributes(); ?>>
 
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-119988896-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-119988896-2');
+</script>
+
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
