44a8afdc227cdd7561f620da7dfb9c430d538923
coloradomedia <piotrek.majkrzak@gmail.com>
2019-06-01 22:18:28 +0200

    404

--- 404.php
+++ 404.php
@@ -8,10 +8,13 @@ get_header(); ?>
 <div class="container-fluid notfound404">
 
 	<h1 class="clip-text cover-404 animated galaxy faster">404</h1>
-	
-	<h2 class="animated fadeIn delay-05s"><?php _e( 'Ups! Istnieje wiele stron w galaktyce, ale ta nie została odnaleziona... :(', 'motywgrafit' ); ?></h2>
 
-	<p class="animated fadeIn delay-1s"><?php _e( 'Wygląda na to, że wpisałeś błędny adres. Sprawdź jeszcze raz.', 'motywgrafit' ); ?></p>
+	<h2 class="animated fadeIn delay-05s">
+		<?php _e( 'Ups! Istnieje wiele stron w galaktyce, ale ta nie została odnaleziona... :(', 'motywgrafit' ); ?>
+	</h2>
+
+	<p class="animated fadeIn delay-1s">
+		<?php _e( 'Wygląda na to, że wpisałeś błędny adres. Sprawdź jeszcze raz.', 'motywgrafit' ); ?></p>
 
 </div>
 
